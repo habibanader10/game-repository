7ea26160f203116f358a1112d193327088c6af3c
hana mostafa shehata <hanonmostafa10@gmail.com>
2026-04-23 23:58:03 +0200

    sock

--- src/game/engine/cells/ContaminationSock.java
+++ src/game/engine/cells/ContaminationSock.java
@@ -1,5 +1,6 @@
 package game.engine.cells;
 
+import game.engine.Constants;
 import game.engine.interfaces.CanisterModifier;
 import game.engine.monsters.Monster;
 
@@ -9,12 +10,19 @@ public class ContaminationSock extends TransportCell implements CanisterModifier
 		super(name, effect);
 	}
 	
-	public  void modifyCanisterEnergy(Monster monster, int canisterValue){
-		monster.alterEnergy(canisterValue);
-		}
-	public void onLand(Monster landingMonster, Monster opponentMonster) {
-    	this.transport(landingMonster); 
-    	this.modifyCanisterEnergy(landingMonster, -100); 
-	}
+	 public void modifyCanisterEnergy(Monster monster, int canisterValue) {
+        monster.alterEnergy(canisterValue);
+    }
+    public void transport(Monster monster) {
+       //backward movement (shield cannot block this)
+        super.transport(monster);
+        //slip penalty (shield-aware via alterEnergy)
+        modifyCanisterEnergy(monster, -Constants.SLIP_PENALTY);
+    }
+
+    public void onLand(Monster landingMonster, Monster opponentMonster) {
+        setMonster(landingMonster);
+        transport(landingMonster);   // now handles movement + penalty together
+    }
 }
 
